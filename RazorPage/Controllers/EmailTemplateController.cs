c2a9c16e28aa218e85eae8b3753865312e90a9a9
TEKCENTURE\vijay.tummala <vijay.tummala@tekzenit.com>
2018-03-07 10:54:06 -0600

    Initial Commit

---
+++ RazorPage/Controllers/EmailTemplateController.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RazorPage.Controllers
+{
+    public class EmailTemplateController : Controller
+    {
+        // GET: EmailTemplate
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult AncillaryProviderInformationTemplate()
+        {
+            StreamReader sr = new StreamReader(@"C:/Users/vijay.tummala/Documents/dataobj.json");
+            string jsonString = sr.ReadToEnd();
+            dynamic obj = JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(jsonString);
+            return View(obj.model);
+        }
+
+        public ActionResult AppointmentNotificationTemplate()
+        {
+            StreamReader sr = new StreamReader(@"C:/Users/vijay.tummala/Documents/dataobj.json");
+            string jsonString = sr.ReadToEnd();
+            dynamic obj = JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(jsonString);
+            return View(obj.model);
+        }
+        public ActionResult AuthorizationForSchedulingTemplate()
+        {
+            StreamReader sr = new StreamReader(@"C:/Users/vijay.tummala/Documents/dataobj.json");
+            string jsonString = sr.ReadToEnd();
+            dynamic obj = JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(jsonString);
+            return View(obj.model);
+        }
+
+        public ActionResult RequestForAncillaryServicesTemplate()
+        {
+            StreamReader sr = new StreamReader(@"C:/Users/vijay.tummala/Documents/dataobj.json");
+            string jsonString = sr.ReadToEnd();
+            dynamic obj = JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(jsonString);
+            return View(obj.model);
+        }
+
+        public ActionResult RequestStatusTemplate()
+        {
+            StreamReader sr = new StreamReader(@"C:/Users/vijay.tummala/Documents/dataobj.json");
+            string jsonString = sr.ReadToEnd();
+            dynamic obj = JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(jsonString);
+            return View(obj.model);
+        }
+
+        public ActionResult RequestSpecialistTemplate()
+        {
+            StreamReader sr = new StreamReader(@"C:/Users/vijay.tummala/Documents/dataobj.json");
+            string jsonString = sr.ReadToEnd();
+            dynamic obj = JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(jsonString);
+            return View(obj.model);
+        }
+
+        public ActionResult ReportAppointmentNotificationTemplate()
+        {
+            StreamReader sr = new StreamReader(@"C:/Users/vijay.tummala/Documents/dataobj.json");
+            string jsonString = sr.ReadToEnd();
+            dynamic obj = JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(jsonString);
+            return View(obj.model);
+        }
+
+        public ActionResult AppointmentToClinic()
+        {
+            StreamReader sr = new StreamReader(@"C:/Users/vijay.tummala/Documents/dataobj.json");
+            string jsonString = sr.ReadToEnd();
+            dynamic obj = JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(jsonString);
+            return View(obj.model);
+        }
+        public ActionResult OnlyRequestsStatus()
+        {
+            StreamReader sr = new StreamReader(@"C:/Users/vijay.tummala/Documents/dataobj.json");
+            string jsonString = sr.ReadToEnd();
+            dynamic obj = JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(jsonString);
+            return View(obj.model);
+        }
+    }
+}
---
+++ RazorPage/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RazorPage.MvcApplication" Language="C#" %>
